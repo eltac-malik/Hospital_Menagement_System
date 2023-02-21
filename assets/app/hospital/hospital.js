41405e2b5ef3c8c804abbf8ff38e22060a3681a6
peqasus777 <peqasus777@gmail.com>
2023-02-21 08:30:26 -0800

    zamir/feature/local-storage-testing

--- assets/app/hospital/hospital.js
+++ assets/app/hospital/hospital.js
@@ -8,3 +8,28 @@
 function fun (){
 window.location.href="doctor-cards.html"
 }
+
+
+
+
+
+
+
+// for Doctor view profile
+
+// localStorage.setItem("doctors", JSON.stringify(doctors));
+
+// function createDoctorProfile(id) {
+//   let allDoctors = JSON.parse(localStorage.getItem("doctors"));
+
+//   let currentDoctor = allDoctors.find((item) => item.doctorId === id);
+//   localStorage.setItem("currentDoctor", JSON.stringify(currentDoctor));
+// }
+
+// function getDoctorProfile() {
+//   window.location.href = "../doctor/doctor-view-profile.html";
+// }
+
+// function getBooking() {
+//   window.location.href = "../doctor/docor-book.html";
+// }
