20bade3e2286cbca279ecae86e65686623c86b03
Farid885 <ferid14zamanli@gmail.com>
2023-02-23 19:52:38 +0400

    login

---
+++ assets/app/log-reg-pro/log-reg-pro.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".zf-login-swiper", {
+    direction: "horizontal",
+    loop: true,
+    autoplay: true,
+    delay: 2500,
+    effect: "cube",
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+  
