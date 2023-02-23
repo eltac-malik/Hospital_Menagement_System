193365e936c27937b487eeed55e5202eb432cd39
AlmammadovAli <almemmedov@mail.ru>
2023-02-23 22:09:11 +0400

    test

--- assets/app/log-reg-pro/profil.js
+++ assets/app/log-reg-pro/profil.js
@@ -1,36 +1,57 @@
-let current = JSON.parse(localStorage.getItem("current"))
 
-
-let name = document.getElementById("name")
-let eMail1 = document.getElementById("email")
-
-let fullName = document.getElementById("fullName")
-let eMail = document.getElementById("eMail")
-let phone = document.getElementById("phone")
-let submitBtn = document.getElementById("submitBtn")
-
-name.textContent = current.name
-eMail1.textContent = current.email
-
-
-submitBtn.addEventListener("click",()=>{
-    let current = JSON.parse(localStorage.getItem("current"))
-
-    let checkName = fullName.value !== '' ? {...current,name:fullName.value} : current;
-    let checkMail = eMail.value !== '' ? { ...checkName, email: eMail.value } : checkName; 
-    let newCurrent = phone.value !== '' ? { ...checkMail, phone: phone.value } : checkMail;
-
-    localStorage.setItem("current", JSON.stringify(newCurrent))
-
-    let allUsers = JSON.parse(localStorage.getItem("users"));
-
-    let newAllUsers = allUsers.map(e=> {
-        if (e.id == newCurrent.id) {
-            e = newCurrent
-        }
-        return e
-    })
-
-    localStorage.setItem("users", JSON.stringify(newAllUsers))
+var mobMenu = document.querySelector('.hc-nav-list-drop');
+var removeBtn = document.querySelector('#hc-remove-mob');
+var mobMenuBtn = document.querySelector('.hc-mob-bar')
+removeBtn.addEventListener('click', ()=>{
+    mobMenu.style.left = '-350px'
+    removeBtn.style.transform = 'rotate(-90deg)'
 
 })
+mobMenuBtn.addEventListener('click', ()=>{
+    mobMenu.style.left = '0'
+    removeBtn.style.transform = 'rotate(90deg)'
+})
+
+
+
+
+
+
+// let current = JSON.parse(localStorage.getItem("current"))
+
+
+// let name = document.getElementById("name")
+// let eMail1 = document.getElementById("email")
+
+// let fullName = document.getElementById("fullName")
+// let eMail = document.getElementById("eMail")
+// let phone = document.getElementById("phone")
+// let submitBtn = document.getElementById("submitBtn")
+
+// name.textContent = current.name
+// eMail1.textContent = current.email
+
+
+// submitBtn.addEventListener("click",()=>{
+//     let current = JSON.parse(localStorage.getItem("current"))
+
+//     let checkName = fullName.value !== '' ? {...current,name:fullName.value} : current;
+//     let checkMail = eMail.value !== '' ? { ...checkName, email: eMail.value } : checkName; 
+//     let newCurrent = phone.value !== '' ? { ...checkMail, phone: phone.value } : checkMail;
+
+//     localStorage.setItem("current", JSON.stringify(newCurrent))
+
+//     let allUsers = JSON.parse(localStorage.getItem("users"));
+
+//     let newAllUsers = allUsers.map(e=> {
+//         if (e.id == newCurrent.id) {
+//             e = newCurrent
+//         }
+//         return e
+//     })
+
+//     localStorage.setItem("users", JSON.stringify(newAllUsers))
+
+// })
+
+
