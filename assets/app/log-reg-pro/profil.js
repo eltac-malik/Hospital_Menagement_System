923ac742ee166d0e37a04691afea8cec1cfb1e65
AlmammadovAli <almemmedov@mail.ru>
2023-02-25 12:09:58 +0400

    ali/bugfix

--- assets/app/log-reg-pro/profil.js
+++ assets/app/log-reg-pro/profil.js
@@ -17,43 +17,43 @@ mobMenuBtn.addEventListener('click', ()=>{
 
 
 
-// let current = JSON.parse(localStorage.getItem("current"))
+let current = JSON.parse(localStorage.getItem("local-users"))
 
 
 let name = document.getElementById("name")
 
-// let eMail1 = document.getElementById("email")
+let eMail1 = document.getElementById("email")
 
-// let fullName = document.getElementById("fullName")
-// let eMail = document.getElementById("eMail")
+let fullName = document.getElementById("fullName")
+let eMail = document.getElementById("eMail")
 // let phone = document.getElementById("phone")
-// let submitBtn = document.getElementById("submitBtn")
+let submitBtn = document.getElementById("submitBtn")
 
-// name.textContent = current.name
-// eMail1.textContent = current.email
+name.textContent = current.name
+eMail1.textContent = current.email
 
 
 submitBtn.addEventListener("click",()=>{
-    // let current = JSON.parse(localStorage.getItem("current"))
+    let current = JSON.parse(localStorage.getItem("local-users"))
 console.log(name.value)
 
 
-    // let checkName = fullName.value !== '' ? {...current,name:fullName.value} : current;
-    // let checkMail = eMail.value !== '' ? { ...checkName, email: eMail.value } : checkName; 
-    // let newCurrent = phone.value !== '' ? { ...checkMail, phone: phone.value } : checkMail;
+    let checkName = fullName.value !== '' ? {...current,name:fullName.value} : current;
+    let checkMail = eMail.value !== '' ? { ...checkName, email: eMail.value } : checkName; 
+    let newCurrent = phone.value !== '' ? { ...checkMail, phone: phone.value } : checkMail;
 
-    // localStorage.setItem("current", JSON.stringify(newCurrent))
+    localStorage.setItem("local-users", JSON.stringify(newCurrent))
 
-    // let allUsers = JSON.parse(localStorage.getItem("users"));
+    let allUsers = JSON.parse(localStorage.getItem("users"));
 
-    // let newAllUsers = allUsers.map(e=> {
-    //     if (e.id == newCurrent.id) {
-    //         e = newCurrent
-    //     }
-    //     return e
-    // })
+    let newAllUsers = allUsers.map(e=> {
+        if (e.id == newCurrent.id) {
+            e = newCurrent
+        }
+        return e
+    })
 
-    // localStorage.setItem("users", JSON.stringify(newAllUsers))
+    localStorage.setItem("users", JSON.stringify(newAllUsers))
 
 })
 
